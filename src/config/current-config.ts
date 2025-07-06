0d24dbd6de1ea11c002a37b53776acdff1f406a9
Vipin Bihari <vipinbiharitiwari15@gmail.com>
2025-07-06 23:54:14 +0530

    update social channel links

--- src/config/current-config.ts
+++ src/config/current-config.ts
@@ -132,10 +132,11 @@ export const BLOG_CONFIG: BlogConfig = {
 
   // सोशल मीडिया
   social: [
-   // { platform: 'twitter', url: `https://twitter.com/gyanso`, label: 'ट्विटर पर फॉलो करें' },
+   { platform: 'twitter', url: `https://x.com/GyanSo_offical`, label: 'ट्विटर पर फॉलो करें' },
    // { platform: 'linkedin', url: `https://linkedin.com/company/gyanso`, label: 'लिंक्डइन पर जुड़ें' },
-   // { platform: 'facebook', url: `https://facebook.com/gyanso`, label: 'फेसबुक पर लाइक करें' },
+   { platform: 'facebook', url: `https://twitter.com/gyanso`, label: 'फेसबुक पर लाइक करें' },
     { platform: 'youtube', url: `https://youtube.com/@gyanso`, label: 'यूट्यूब पर सब्सक्राइब करें' },
+    { platform: 'instagram', url: `https://instagram.com/gyanso_official/`, label: 'इंस्टाग्राम पर फॉलो करें' },
   ],
 
   // SEO
